2b94f792f7a3c2fbea43c38e2c4e8b10f6dfffeb
chiyu1203 <chiyu1203@gmail.com>
2024-10-20 22:49:37 +0200

    add plotting trajectory for animal by animal; add classification for active trials; add circular plots

--- create_analysis_method.py
+++ create_analysis_method.py
@@ -2,15 +2,16 @@ import json
 
 file_name = "analysis_methods_dictionary.json"
 analysis_methods = {
-    "experiment_name": "band",
+    "experiment_name": "choice",
     "overwrite_curated_dataset": True,
     "save_output": True,
-    "agents_shared_across_vrs": False,
-    "time_series_analysis": False,
+    "agents_shared_across_vrs": True,
+    "time_series_analysis": True,
     "filtering_method": "sg_filter",
     "plotting_trajectory": True,
     "load_individual_data": True,
     "select_animals_by_condition": True,
+    "active_trials_only": True,
     "graph_colour_code": ["r", "b", "g", "k", "c", "y", "m", "r"],
     "camera_fps": 100,
     "trackball_radius_cm": 0.5,
